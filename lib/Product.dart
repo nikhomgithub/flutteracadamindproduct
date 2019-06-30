570b7cee8c38bd7a057ec58017e47a57cb514518
nikhomgithub <mr_nikom@yahoo.com>
2019-06-30 19:46:32 +0700

    forth

--- lib/Product.dart
+++ lib/Product.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
 
 class Product extends StatelessWidget{
-  final String newproduct;
+  final List<String> newproducts;
 
-  Product({this.newproduct});
+  Product({this.newproducts}){
+    print("Product constructor");
+  }
 
   @override
   Widget build(BuildContext context) {
+    print("Product build");
     return Card(
-                  child:Column(
-                    children: <Widget>[
-                      Image.asset('./images/captain.png'),
-                      Text(newproduct)
-                    ],
-                  )
-                );
+      child:Column(children:newproducts.map((element)=>
+        Card(
+          child:Column(
+            children: <Widget>[
+              Image.asset('./images/captain.png'),
+              Text(element)
+            ],
+          )
+        )
+        ).toList()
+      )
+    );
   }
 }
